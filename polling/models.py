3baa0ece9eda3ee94aa7af4a9bde7a21793d4a63
Mintae <mtwow789@hotmail.com>
2022-01-22 21:51:40 -0800

    33

--- polling/models.py
+++ polling/models.py
@@ -4,7 +4,7 @@ from django.db import models
 class Poll(models.Model):
     title = models.CharField(max_length=128)
     text = models.TextField(blank=True)
-    score2 = models.IntegerField(default=0)
+    score = models.IntegerField(default=0)
 
     def __str__(self):
         return self.title
